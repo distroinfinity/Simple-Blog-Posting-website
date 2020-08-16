8cb67b5a6ab55ce9514c39c54db08e0a9de6b9eb
Manu Rajput <manurajput2911@gmail.com>
2020-08-16 12:01:41 +0530

    First and Finl commit

---
+++ personal/models.py
@@ -0,0 +1,20 @@
+# from django.db import models
+
+# PRIORITY = [
+# 	('L', 'Low'), #Tuple1
+# 	('M', 'Medium'), #Tuple2
+# 	('H', 'High'), #Tuple3
+# ]
+
+# class Question(models.Model):
+# 	title		 			= models.CharField(max_length=60)
+# 	question 				= models.TextField(max_length=400)
+# 	priority 				= models.CharField(max_length=1, choices=PRIORITY)
+
+# 	def __str__(self):
+# 		return self.title
+
+
+# 	class Meta:
+# 		verbose_name = 'The Question'
+# 		verbose_name_plural = 'Questions from People'
---
+++ personal/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from operator import attrgetter
+from blog.views import get_blog_queryset
+from blog.models import BlogPost
+
+
+
+def home_screen_view(request):
+	
+	context = {}
+	query = ""
+	if request.GET:
+		query = request.GET['q']
+		context['query'] = str(query)
+	
+	blog_posts = sorted(get_blog_queryset(query), key=attrgetter('date_updated'), reverse=True)
+	context['blog_posts'] = blog_posts
+
+	return render(request, "personal/home.html", context)
